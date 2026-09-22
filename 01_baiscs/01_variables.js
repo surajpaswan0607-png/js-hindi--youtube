d2d12569af06d92afb54781f1d39fd07bfb792bd
surajpaswan0607-png <surajpaswan0607@gmail.com>
2026-09-22 18:02:04 +0000

    var, let, console

---
+++ 01_baiscs/01_variables.js
@@ -0,0 +1,21 @@
+const accountId= 12345 //contant declaration
+let accountEmail=" surajpaswan0607@gmil.com"  //variables declaration
+let accountEmail2= "surajpaswan0607@gmil.com"
+var accountPassword1 = "123456"   // varaibles declaration
+var accountPassword2 = 123456
+accountCity = "lucknow"    // automatically defines the datatype of identifier if not given (generally not suggested and considered)
+let accountState  //undefined data type- if not given the values to variable
+
+//updation
+accountEmail= "srjtkd0607@gmail.com"    
+accountPassword1= 9076931119
+accountCity= "bhopal"
+
+//printing 
+console.log(accountId) //for printing/ returning single identifiers
+console.table([accountEmail, accountEmail2, accountCity, accountState ])
+
+// for multiple idenifiers in form of a table
+
+
+//
