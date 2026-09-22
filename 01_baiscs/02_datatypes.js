f4ae236a1690002f6fe98e663d9e59dad8b8253f
surajpaswan0607-png <surajpaswan0607@gmail.com>
2026-09-22 18:59:42 +0000

    datatypes

---
+++ 01_baiscs/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict "; /* this indicates that we will use new version/ syntax/ and codes 
+               from new version of javascript */
+
+// alert(3+3)  //browser only function -this is a simple alert box which will show the result of 3+3 which is 6
+
+let name= " suraj" //string data type
+let age= 20 //number data type
+let isLoggedIn= true //boolean data type
+let state= null //null data type
+let city //undefined data type
+
+//DATATYPES
+//number - 2 to the power of 53 -1
+//bigint for large numbers
+// string - sequence of characters
+//boolean - true or false
+//null - empty value
+//undefined - variable declared but not assigned any value
+//symbol - unique and immutable data type
+//object - collection of key-value pairs
+
+console.log(typeof name) //this will print the data type of the variable name in the console
+console.log(typeof undefined) // undefined data type
+console.log(typeof null) //object data type
