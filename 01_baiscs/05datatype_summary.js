52d53f64e17c47e9975ad9c19e43787d7cde6b54
surajpaswan0607-png <surajpaswan0607@gmail.com>
2026-09-23 23:14:55 +0000

    datatype summary

---
+++ 01_baiscs/05datatype_summary.js
@@ -0,0 +1,44 @@
+//primitive
+//7 types: string, number, boolean, null, undefined, symbol, bigInt
+
+// dynamic typing-we do not specify the varaible type- it is automatically assigned
+
+// const score= 100
+// const scorevalue=100.3
+// const isLoggedIn= false
+// const OutsideTemp=null
+// let userEmail; //undefined
+
+// //symbol
+// const id= Symbol('123')
+// const AnotherId= Symbol('123') //both are different 
+
+// console.log(id== AnotherId) //false 
+
+// bigInt
+
+const num= 5656264161649646645654496495n
+console.log(typeof num) //bigint 
+
+
+// Reference(Non primitive)
+//Array, object, functions
+
+const heroes =["shaktiman", "naagraj", "doga"] //array
+
+//objects- key value pair
+
+let myObj={
+    name: suraj,
+    surname: Paswan,
+    age: 22
+}
+
+
+//function- in js we can store function as a variable
+
+const myFunction = function(){
+    console.log()
+}
+
+
